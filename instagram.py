6cdb9ebb1b33688d2f039fe61f09ab946131a78d
Wellington Silverio <wellington.alves.silverio@gmail.com>
2019-07-17 13:26:43 -0300

    .

--- instagram.py
+++ instagram.py
@@ -10,7 +10,6 @@ class Instagram:
 
     def open(self):
         self.browser = webdriver.Chrome("./chromedriver")
-        self.browser.get("http://instagram.com/")
 
     def getBrowser(self):
         return self.browser
@@ -23,8 +22,7 @@ class Instagram:
         return self.browser
     
     def login(self, username, password):
-        buttonGoLogin = self.delayBrowser().find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[2]/p/a')
-        buttonGoLogin.click()
+        self.browser.get("https://www.instagram.com/accounts/login/?source=auth_switcher")
 
         inputUsername = self.delayBrowser().find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div/div/form/div/div/div/input[@name="username"]')
         inputUsername.send_keys(username)
@@ -34,14 +32,16 @@ class Instagram:
 
         buttonLogin = self.delayBrowser().find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button')
         buttonLogin.click()
+
+        input("Press Enter to continue...")
     
     def getPhotosIdByTag(self, tag):
         photosLink = []
-        self.delayBrowser().get("https://www.instagram.com/explore/tags/" + tag)
+        self.browser.get("https://www.instagram.com/explore/tags/" + tag)
 
-        for r in range(0, 10):
+        for r in range(0, 5):
             self.delayBrowser().execute_script("window.scrollTo(0, 10000);")
-            images = self.delayBrowser().find_elements_by_xpath('//*[@id="react-root"]/section/main/article/div/div/div/div/a')
+            images = self.delayBrowser().find_elements_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div/div[1]/div/a')
 
             for link in images:
                 photosLink.append(link.get_attribute("href"))
